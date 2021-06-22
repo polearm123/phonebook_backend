a1d58aac8e0b24d20ff013d15f5a99fac8ee5a8e
polearm123 <shaggywaggy29@gmail.com>
2021-06-22 16:36:45 +0100

    added error handling middleware and bug fixes for all data requests

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+
+//imports the person module from the models directory
 const Person = require('./models/person')
+const { mongo } = require('mongoose')
 require('dotenv').config()
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
-
-//imports the person module from the models directory
-
 
 
 
@@ -48,43 +48,52 @@ app.get('/' , (request,response) => {
 
 
 //info route handler displays information from the people list in HTML format
-app.get('/info', (request,response) => {
+app.get('/api/people/info', (request,response,next) => {
   const date = new Date()
-  response.send(`<h1>Phonebook has info for ${people.length} people</h1>
-    <p>${date}</p>
-  `)
+  Person.find({}).countDocuments().then(infoResponse => {
+    response.send(`<h1>Phonebook has info for ${infoResponse} people</h1>
+    <p>${date}</p>`
+  )
+  }).catch(error =>
+    next(error))
+
+    mongoose.connection.close()
+  })
+
+
+//retrieves a specific document from the database corresponding to the
+//request paramters
+app.get('/api/people/:id', (request,response,next) => {
+
+  Person.findById(request.params.id).then(person => {
+    if(note){
+      response.json(person)
+    }else{
+      response.status(404).end()
+    }
+    //catch block will trigger if the id given
+    //does not match the mongodb id format
+  }).catch(error => next(error))
 
 })
 
 
-//gets a specific person object from the people list 
-//uses Number to convert string to integer to find the person in 
-//the people list
-app.get('/api/people/:id', (request,response) => {
-  console.log("request params: " , request.params)
-  const id = Number(request.params.id)
-  
-  const matchingPerson = people.find((person) => person.id === id)
+//to handle editing contacts that already exist
+app.put('/api/people/:id',(request,response,next) => {
+  const body = request.body
+  console.log(body._id)
 
-  if(matchingPerson){
-    response.json(matchingPerson)
-  }else{
-    console.log("the id requested is not present in the database")
-    response.status(404).end()
+  const updatedPerson = {
+    number: body.number,
+    name: body.name
   }
 
+  Person.findByIdAndUpdate(body._id,updatedPerson,{new:true}).then(updatedResponse => {
+    console.log("found a match", updatedResponse)
+    response.json(updatedResponse)
+  }).catch(error => next(error))
+
 })
-
-
-//used to generate an id for the json list of people and numbers
-//finds the max id in the list adds one and returns it
-const generateId = () => {
-
-  const maxId = people.length >0 ? Math.max(...people.map((person)=>person.id)) : 0
-  return maxId+1
-
-} 
-
 
 //route for post request made to server
 //checks if the name or number is present if so returns 
@@ -94,53 +103,55 @@ app.post('/api/people', (request,response) => {
 
   const body = request.body
 
-  console.log("body of post request is:  " , body.name)
-  if(!body.name){
+  console.log(`number is ${body.number} name is $`)
+  if(body.name === undefined){
     console.log("person could not be added request body empty")
     return response.status(404).end()
   }
-  //check if the name or number exists in the phonebook
-  const existingNumber = people.find(person => person.number === body.number) 
-  const existingName = people.find(person=>person.name===body.name)
+  
+  
+  //if either the name or number exist an error is sent
+  //in a response object
+  Person.find({$or: [{name:body.name},{number:body.number}]}
+  ).then(foundPerson => {
+    console.log("response from matching person" , response)
+    if(foundPerson){
+      console.log("existing person with name or number")
+      return response.status(404).json({error:"must be unique"})
+    }
+    mongoose.connection.close()
+  })
 
-  //returns an error json if the name or number already exists
-  if(existingNumber||existingName){
-    console.log("existing person with name or number")
-    return response.status(404).json({error:"must be unique"})
-  }
 
-  const newPerson = {
-    id:generateId(),
+  const newPerson = new Person({
     name:body.name,
     number:body.number
-  }
+  })
 
-  people = people.concat(newPerson)
-  console.log("person successfully added")
-  response.status(204).end()
-
-
+  //save the person specified using the mongoose model
+  //to the database and close the connection
+  newPerson.save().then(result => {
+      console.log("new person saved" , newPerson)
+      return response.status(204).end()
+  }).catch(error => {
+      console.log("error in saving person",error.message)
+      // mongoose.connection.close()
+      return response.status(404).end()
+    })
+    mongoose.connection.close()
 })
 
+    
 
 //route handling a delete request
 //finds the person in the list and deletes returning a 204 status
 //if person is not present it returns a 404 status
-app.delete('/api/people/:id' , (request,response) => {
+app.delete('/api/people/:id' , (request,response,next) => {
 
-  const id = Number(request.params.id)
-  console.log("id is: " , id)
-  const findPerson = people.find((person) => person.id === id)
-  console.log("person found is: ", findPerson)
-  if(findPerson){
-    people = people.filter((person) => person.id != id)
-    console.log("person found and deleted")
+  Person.findByIdAndDelete(request.params.id).then(result => {
     response.status(204).end()
-  }else{
-    console.log("person does not exist and cannot be deleted")
-    response.status(404).end()
-
-  }
+    //leaves the error handling to the error handling middleware
+  }).catch(error => next(error))
 
 })
 
@@ -158,12 +169,24 @@ app.get('/api/people', (request,response) => {
 const unknownEndpoints = (request,response) => {
   response.status(404).send({error:"unknown endpoint"})
 }
-
-
-
 app.use(unknownEndpoints)
 
 
+//error handling middleware
+//express error handlers are middleware that are defined with a function
+const errorHandler = (error,request,response,next) => {
+  console.log(error.message)
+
+  if(error.name==='CastError'){
+    return response.status(400).send({error:'malformatted id'})
+  }
+}
+app.use(errorHandler)
+
+
+//after setup the PORT uses the PORT variable set in the .env file.
+//put in .gitignore to avoid sensitive information like db passwords from being revealed to public
+//back-end listens on port stated waiting for axios requests!
 const PORT = process.env.PORT
 app.listen(PORT , () => {
 	console.log(`server is running on port ${PORT}`)
