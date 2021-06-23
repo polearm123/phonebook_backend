771b6dfceb6e9f23edeefca6a08c28902b49e0ae
polearm123 <shaggywaggy29@gmail.com>
2021-06-23 16:26:55 +0100

    changed error messages to ones inside next(error) function

--- index.js
+++ index.js
@@ -163,11 +163,11 @@ const errorHandler = (error,request,response,next) => {
   console.log(error.message)
 
   if(error.name==='CastError'){
-    return response.status(400).send({error:'malformatted id'})
+    return response.status(400).send({error:error.message})
   }else if(error.name==='ValidationError'){
-    return response.status(400).send({error:'validation error'})
+    return response.status(400).send({error:error.message})
   }else if(error.name==='CreateError'){
-    return response.status(400).send({error:"error in creation"})
+    return response.status(400).send({error:error.message})
   }
 }
 app.use(errorHandler)
